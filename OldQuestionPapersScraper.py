3665bb2dafc26bc3ceca3816e1e07b88fcc0bcc8
Isham Mahajan <mahajan.isham@gmail.com>
2019-04-16 20:56:10 +0530

    scraper: actually make it work -- with hard coded values.

--- OldQuestionPapersScraper.py
+++ OldQuestionPapersScraper.py
@@ -5,35 +5,52 @@ from selenium.webdriver.chrome.options import Options
 import urllib.request
 
 # Initialising options for downloading
-years = [2016, 2017, 2018]
+site = "http://1.186.28.31/MIT/Question%20Paper.aspx"
+years = [2015, 2016, 2017, 2018]
 timeline = ["Jun "]
 
 options = Options()
 options.add_argument("--headless")
 driver = webdriver.Chrome(options=options)
-
-driver.get("http://1.186.28.31/MIT/Question%20Paper.aspx")
-# driver.find_element_by_id("ctl00_ctl00_chmain_MITContent_FileGridCS_gvFiles_ctl14_lbFolderItem").click()
-yearElements = driver.find_elements_by_xpath("//a")
-for yearElement in yearElements:
-    try:
-        if int(yearElement.text) in years:
-            year = yearElement.text
-            yearElement.click()
-            seasonElements = driver.find_elements_by_xpath("//a")
-            for seasonElement in seasonElements:
-                if seasonElement.text == (" Jun" + year):
-                    seasonElement.click()
-                    semElements = driver.find_elements_by_xpath("//a")
-                    for semElement in semElements:
-                        if semElement.text == " IV Sem":
-                            semElement.click()
-                            print(semElement.text)
-            driver.back()
-            print(year)
-    except ValueError:
-        continue
-print(driver.title)
+driver.get(site)
 
 
-# urllib.request.urlretrieve("http://1.186.28.31/RootFolder/2018/June%202018/IV%20Sem/Information%20and%20Communication%20Technology%20Eng/Computer%20Organization%20and%20Microprocessor%20System%20(ICT%202202)%20RCS.pdf", "test.pdf")
+def search(finishedYears):
+    # Resetting the state of the web page to beginning
+    driver.get(site)
+    year = driver.find_elements_by_xpath("//a")
+    for yearElement in year:
+        try:
+            if int(yearElement.text) in list(set(years) - set(finishedYears)):
+                currentYear = yearElement.text
+                yearElement.click()
+                seasons = driver.find_elements_by_xpath("//a")
+                for seasonElement in seasons:
+                    if "jun" in str(seasonElement.text).lower():
+                        seasonElement.click()
+                        sems = driver.find_elements_by_xpath("//a")
+                        for semElement in sems:
+                            if semElement.text == " IV Sem":
+                                semElement.click()
+                                branches = driver.find_elements_by_xpath("//a")
+                                for branchElement in branches:
+                                    if "information" in str(branchElement.text).lower():
+                                        branchElement.click()
+                                        papers = driver.find_elements_by_xpath("//a")
+                                        print("Downloading papers for:" + currentYear)
+                                        for paperElement in papers:
+                                            if "systems" in str(paperElement.text).lower():
+                                                urllib.request.urlretrieve(paperElement.get_attribute("href"),
+                                                                           "/Users/isham/QuestionPapers/CN/" + str(
+                                                                               currentYear).strip() + "_" + str(
+                                                                               paperElement.text))
+                                                print(paperElement.text)
+                                        print("\n")
+                                        search(list(set(finishedYears).union({int(currentYear)})))
+                                        return
+
+        except ValueError:
+            continue
+
+
+search([])
